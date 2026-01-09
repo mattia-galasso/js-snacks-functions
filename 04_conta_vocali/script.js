38f51e70d0102cb9ba6d44190dfb4adf621ff759
MattiaGalasso <galasso.mattia94@gmail.com>
2026-01-09 19:17:10 +0100

    add Four Exercise

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,28 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
+const vowels = ["a", "e", "i", "o", "u"];
 
 // Dichiara la funzione qui.
-
+/**
+ * This function takes a word and returns the number of vowels.
+ * @param {string} isWord Word for vowel counting
+ * @param {array} isVowels Array with vowels
+ * @returns {number} Number of vowels
+ */
+function hasVowals(isWord, isVowels) {
+  let numberVowals = 0;
+  for (let i = 0; i < isWord.length; i++) {
+    const currentChar = isWord[i];
+    if (isVowels.includes(currentChar)) {
+      numberVowals++;
+    }
+  }
+  return numberVowals;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const wordHasVowals = hasVowals(word, vowels);
 
-
-
+console.log("Word: ", word, "|", "Number of Vowels: ", wordHasVowals);
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
