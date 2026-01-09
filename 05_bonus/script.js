1d29b9442b80d64ed0b1174c994ab78d15982104
MattiaGalasso <galasso.mattia94@gmail.com>
2026-01-09 22:36:14 +0100

    Add Fifth Exercise

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,34 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+const userName = "Mario";
+const todayDate = new Date();
+let currentHour = todayDate.getHours();
+let outputMessage = "";
+console.log("todayDate: ", todayDate, typeof todayDate);
+console.log("currentHour: ", currentHour, typeof currentHour);
 
 // Dichiara la funzione qui.
-
+/**
+ * This function takes the name, checks what time it is and returns a greeting message
+ * @param {string} isUserName Name of the person to greet
+ * @param {number} hour Current time
+ * @returns {string} Output Message
+ */
+function greet(isUserName, hour) {
+  if (hour <= 13) {
+    outputMessage = "Buongiorno" + " " + userName;
+  } else if (hour <= 17) {
+    outputMessage = "Buon Pomeriggio" + " " + userName;
+  } else {
+    outputMessage = "Buona Sera" + " " + userName;
+  }
+  return outputMessage;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const userGreet = greet(userName, currentHour);
 
-
+console.log(userGreet);
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
