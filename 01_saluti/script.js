a9e9c92508482506d0976f9d39790ea15c77a9c0
MattiaGalasso <galasso.mattia94@gmail.com>
2026-01-09 15:02:42 +0100

    Add Second Exercise

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -3,6 +3,11 @@
 const userName = "Mario";
 
 // Dichiara la funzione qui.
+/**
+ * This function takes a Name and returns a greeting with the addition of "Ciao"
+ * @param {string} word Name to add to "Ciao"
+ * @returns {string} Returns "Ciao [Name]"
+ */
 const userGreet = (word) => (isGreet = "Ciao" + " " + word);
 
 // Invoca la funzione qui e stampa il risultato in console
--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,25 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+/**
+ * This function takes an array and returns the initials of each word.
+ * @param {array} nameArray Array populated by Words
+ * @returns {array} Array with initials
+ */
+function isInitials(nameArray) {
+  const initials = [];
+  for (let i = 0; i < nameArray.length; i++) {
+    const currentName = nameArray[i];
+    isNameInit = currentName[0];
+    initials.push(isNameInit);
+  }
+  return initials;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const isNamesInitials = isInitials(names);
 
-
-
+console.log(names);
+console.log(isNamesInitials);
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
